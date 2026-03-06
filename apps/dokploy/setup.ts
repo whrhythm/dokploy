707137a235e0ef830b25282b8e24453e3946a37d
jiang zhenggang <1321670187@qq.com>
2026-03-06 18:42:12 +0800

    默认从docker.1ms.run获取镜像

--- apps/dokploy/setup.ts
+++ apps/dokploy/setup.ts
@@ -1,38 +1,41 @@
-import { exit } from "node:process";
 import { exec } from "node:child_process";
+import { exit } from "node:process";
 import { promisify } from "node:util";
 
 const execAsync = promisify(exec);
+
 import { setupDirectories } from "@dokploy/server/setup/config-paths";
 import { initializePostgres } from "@dokploy/server/setup/postgres-setup";
 import { initializeRedis } from "@dokploy/server/setup/redis-setup";
 import {
-	initializeNetwork,
-	initializeSwarm,
+  initializeNetwork,
+  initializeSwarm,
 } from "@dokploy/server/setup/setup";
 import {
-	createDefaultMiddlewares,
-	createDefaultServerTraefikConfig,
-	createDefaultTraefikConfig,
-	initializeStandaloneTraefik,
-	TRAEFIK_VERSION,
+  createDefaultMiddlewares,
+  createDefaultServerTraefikConfig,
+  createDefaultTraefikConfig,
+  initializeStandaloneTraefik,
+  TRAEFIK_VERSION,
 } from "@dokploy/server/setup/traefik-setup";
 
 (async () => {
-	try {
-		setupDirectories();
-		createDefaultMiddlewares();
-		await initializeSwarm();
-		await initializeNetwork();
-		createDefaultTraefikConfig();
-		createDefaultServerTraefikConfig();
-		await execAsync(`docker pull traefik:v${TRAEFIK_VERSION}`);
-		await initializeStandaloneTraefik();
-		await initializeRedis();
-		await initializePostgres();
-		console.log("Dokploy setup completed");
-		exit(0);
-	} catch (e) {
-		console.error("Error in dokploy setup", e);
-	}
+  try {
+    setupDirectories();
+    createDefaultMiddlewares();
+    await initializeSwarm();
+    await initializeNetwork();
+    createDefaultTraefikConfig();
+    createDefaultServerTraefikConfig();
+    await execAsync(
+      `docker pull docker.1ms.run/library/traefik:v${TRAEFIK_VERSION}`,
+    );
+    await initializeStandaloneTraefik();
+    await initializeRedis();
+    await initializePostgres();
+    console.log("Dokploy setup completed");
+    exit(0);
+  } catch (e) {
+    console.error("Error in dokploy setup", e);
+  }
 })();
