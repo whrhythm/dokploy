abd2e725f3a5b22f4baa6e634c0a86cac72597a1
Your Name <you@example.com>
2026-03-17 10:36:48 +0800

    修改项目favicon

--- apps/dokploy/pages/_document.tsx
+++ apps/dokploy/pages/_document.tsx
@@ -1,37 +1,37 @@
 import NextDocument, {
-	type DocumentContext,
-	type DocumentInitialProps,
-	Head,
-	Html,
-	Main,
-	NextScript,
+  type DocumentContext,
+  type DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
 } from "next/document";
 
 type DokployDocumentProps = DocumentInitialProps & {
-	locale?: string;
+  locale?: string;
 };
 
 export default function Document({ locale }: DokployDocumentProps) {
-	return (
-		<Html lang={locale ?? "zh-Hans"} className="font-sans">
-			<Head>
-				<link rel="icon" href="/icon.svg" />
-			</Head>
-			<body className="flex h-full w-full flex-col font-sans">
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang={locale ?? "zh-Hans"} className="font-sans">
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <body className="flex h-full w-full flex-col font-sans">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
 Document.getInitialProps = async (
-	ctx: DocumentContext,
+  ctx: DocumentContext,
 ): Promise<DokployDocumentProps> => {
-	const initialProps = await NextDocument.getInitialProps(ctx);
+  const initialProps = await NextDocument.getInitialProps(ctx);
 
-	return {
-		...initialProps,
-		locale: ctx.locale,
-	};
+  return {
+    ...initialProps,
+    locale: ctx.locale,
+  };
 };
